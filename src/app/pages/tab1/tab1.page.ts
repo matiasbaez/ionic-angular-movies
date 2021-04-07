b99098381e7cb0706619ffde5a371b881061aeb4
Matias Báez <matiasbaez@users.noreply.github.com>
2021-04-07 16:22:50 -0400

    Updated, list movies and show details

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { MoviesService } from 'src/app/services/movies.service';
+
+import { Movie } from 'src/app/interfaces/interfaces';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +11,30 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  recentMovies: Movie[] = [];
+  popular: Movie[] = [];
+
+  constructor(
+    private moviesService: MoviesService
+  ) {}
+
+  ngOnInit() {
+    this.moviesService.getFeature().subscribe(res => {
+      this.recentMovies = res.results;
+    });
+
+    this.getPopular();
+  }
+
+  loadMore() {
+    this.getPopular();
+  }
+
+  getPopular() {
+    this.moviesService.getPopular().subscribe(res => {
+      const temp = [...this.popular, ...res.results];
+      this.popular = temp;
+    });
+  }
 
 }
